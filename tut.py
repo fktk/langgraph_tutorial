6569551c4f1d8d55ba57d4b59926df5789dd1643
fktk <windwild12@gmail.com>
2025-01-02 09:01:09 +0900

    LLMをollamaに変更

--- tut.py
+++ tut.py
@@ -1,5 +1,3 @@
-import os
-import json
 import multiprocessing
 
 from dotenv import load_dotenv
@@ -7,7 +5,7 @@ from typing import Annotated
 from typing_extensions import TypedDict
 from pprint import pprint
 
-from langchain_community.chat_models import ChatLlamaCpp
+from langchain_ollama import ChatOllama
 from langchain_community.tools.tavily_search import TavilySearchResults
 
 from langgraph.graph import StateGraph
@@ -22,32 +20,28 @@ class State(TypedDict):
     messages: Annotated[list, add_messages]
 
 
-llm = ChatLlamaCpp(
+llm = ChatOllama(
+    model='qwen2.5:q4',
     temperature=0.1,
-    model_path='./models/qwen2.5-3b-instruct-q4_k_m.gguf',
-    n_ctx=10000,
-    n_gpu_layers=20,
-    n_batch=300,
-    max_tokens=512,
-    n_threads=multiprocessing.cpu_count() - 1,
+    num_thread=multiprocessing.cpu_count() - 1,
     repeat_penalty=1.5,
     top_p=0.9,
     verbose=False,
 )
-
-pprint('start-----------')
 
 tool = TavilySearchResults(max_results=2)
 tools = [tool]
 
 llm_with_tools = llm.bind_tools(
     tools=tools,
-    tool_choice={'type': 'function', 'function': {'name': 'tavily_search_results_json' }},
+    # tool_choice={'type': 'function', 'function': {'name': 'tavily_search_results_json' }},
 )
+
+# pprint(llm_with_tools.invoke('名古屋の天気を調べて'))
 
 
 def chatbot(state: State):
-    return {'messages': [llm.invoke(state['messages'])]}
+    return {'messages': [llm_with_tools.invoke(state['messages'])]}
 
 
 graph_builder = StateGraph(State)
@@ -61,16 +55,23 @@ graph_builder.add_conditional_edges(
 graph_builder.add_edge('tools', 'chatbot')
 graph_builder.set_entry_point('chatbot')
 
-graph = graph_builder.compile(checkpointer=MemorySaver())
+graph = graph_builder.compile(
+    checkpointer=MemorySaver(),
+    # interrupt_before=['tools'],
+)
 
-# graph.get_graph().draw_mermaid_png(output_file_path='./graph.png')
+graph.get_graph().draw_mermaid_png(output_file_path='./graph.png')
 print(graph.get_graph().draw_ascii())
 
 def stream_graph_updates(user_input: str):
+    messages = None if user_input == '' else [('user', user_input)]
     config = {'configurable': {'thread_id': '1'}}
-    for event in graph.stream({'messages': [('user', user_input)]}, config):
-        for value in event.values():
-            print('Assistant:', value['messages'][-1].content)
+    events = graph.stream({'messages': messages}, config, stream_mode='values')
+    for event in events:
+        if 'messages' in event:
+            event['messages'][-1].pretty_print()
+    snapshot = graph.get_state(config)
+    print(snapshot.next)
 
 while True:
     try:
